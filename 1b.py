1f0598a6c494d889a0ead9efb8f8d20fe601e02e
Jan Charatan <jan.charatan@gmail.com>
2025-12-02 20:51:37 -0500

    part 1b

--- 1b.py
+++ 1b.py
@@ -8,16 +8,28 @@ def main():
         for l in f.readlines():
             direction, scale = l[0], int(l[1:])
 
+            if scale > 100:
+                ans += scale // 100
+
+            remainder = scale % 100
+
+            start = curr
             if direction == "L":
+                
+                if start != 0 and curr - remainder <= 0:
+                    ans += 1
+
                 curr -= scale
+
             elif direction == "R":
+                if start != 0 and curr + remainder >= 100:
+                    ans += 1
+
                 curr += scale
-            
+
             curr %= 100
-            
-            if curr == 0:
-                ans += 1
-    
+            print(curr, ans)
+
     print(f"The answer is {ans}.")
 
 
